ef8a9c71c30ceb9ed433cc8f361309bf55712557
femresirvan <femresirvan@gmail.com>
2021-01-27 06:05:23 +0300

    artık sorular hep farkli geliyor

--- public/js/randomArray.js
+++ public/js/randomArray.js
@@ -5,23 +5,23 @@ const imgArray = ['2', '3', '5', '6', '7', '8', '12', '16', '29', '42', '45', '9
 let questionArray = new Array();
 for (let i = 0; i < 5; i++) {
     let a = Math.floor(Math.random() * 12); // 11 ye kadar random sayı seçiyor
-    questionArray[i] = imgArray[a]; 
-    
-    // for (;;) {
-    //     if(i == 0) break;
-    //     for(let j = 0; j < i; j++){
-    //     if (questionArray[j] == imgArray[a]) {
-    //         a = Math.floor(Math.random() * 14);
-    //         questionArray[i] = imgArray[a];
-    //     } else break;}
-    // }
-}
+    questionArray[i] = imgArray[a];
 
+    for (;;) {
+        for (let j = 0; j < i; j++) {
+            if (questionArray[j] == imgArray[a]) {
+                a = Math.floor(Math.random() * 14);
+                questionArray[i] = imgArray[a];
+            }
+        }
+        break;
+    }
+}
 
     document.getElementById(questionArray[count]).classList.remove('d-none');
 
 
 
 
-// document.getElementById("MyElement").classList.add('MyClass'); CLASS EKLEME
-// document.getElementById("MyElement").classList.remove('MyClass'); CLASS SİLME
+    // document.getElementById("MyElement").classList.add('MyClass'); CLASS EKLEME
+    // document.getElementById("MyElement").classList.remove('MyClass'); CLASS SİLME
